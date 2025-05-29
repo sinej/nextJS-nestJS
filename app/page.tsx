b1d73b4f7838aca9f0e84a6106656d390053f0b8
sinej <sinej1020@gmail.com>
2025-05-29 09:25:58 +0900

    feat: meta 데이터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Next.js Tutorial',
+  description: 'Next.js 튜토리얼',
+}
+
 export default function Home() {
   return (
-    <main>Home</main>
+    <main>
+      <Link href={'/dashboard'}>대시보드</Link>
+    </main>
   );
 }
